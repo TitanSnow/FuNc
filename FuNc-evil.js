bad2d0990ac2b4385920a47c9cc1352f948e6e8c
TitanSnow <sweeto@live.cn>
2017-02-09 16:22:01 +0800

    add comment

--- FuNc-evil.js
+++ FuNc-evil.js
@@ -1,9 +1,32 @@
+// FuNc-evil.js
+// FuNc evaler (core part)
+// lexical analysis & code evaluation
 "use strict"
+
+// v is the string ready to eval, a is the "global"
+// "global" means the whole thing script runs in
 var v,a
+
+// df is the regex to match token
+// there is three type of token in FuNc
+// 1. space -- always ignore
+// 2. real identifier -- \w\$
+// 3. fake identifier -- not real ones
+// both real and fake identifier can be a identifier
+// but can't mix as one token
 var df=/\s*([\w\$]+|[^\s\w\$]+)/g
+
+// define the error class
+// base class EvilError
+// EOF for end of file(string)
+// SyntaxError for its name
+// (maybe there won't be a syntax error because there's no syntax?)
+// TODO add a constructor to store error message
 class EvilError{}
 class EOF extends EvilError{}
 class SyntaxError extends EvilError{}
+
+// operation to v
 function setv(pv){
 	v=pv
 	df.lastIndex=0
@@ -11,35 +34,47 @@ function setv(pv){
 function getv(){
 	return v
 }
+
+// operation to a
 function seta(pa){
 	a=pa
 }
 function geta(){
 	return a
 }
+
+// lexical analysis
 function nxttok(){
-	var li=df.lastIndex
+	var li=df.lastIndex				// store lastIndex to check next match
 	var rst=df.exec(v)
-	if(rst===null)
+	if(rst===null)					// null just means EOF because the right regex
 		throw new EOF()
-	if(rst.index!=li)
+	if(rst.index!=li)				// I can't find such a situation
 		throw new SyntaxError()
 	return rst[1]
 }
+
+// get a "global", return a lookup function
+// to make this is to let users config it
 var get_lookup=function(obj){
 	return obj.rt.lookup
 }
+
+// evaluation
 function nxtfun(){
 	var tok,fun,len,arg,ifo
-	tok=nxttok()
-	ifo=get_lookup(a)(a,tok)
+	tok=nxttok()				// get token
+	ifo=get_lookup(a)(a,tok)	// get the obj info
 	fun=ifo.func
 	len=ifo.len
 	arg=[]
 	while(len--)
-		arg.push(nxtfun())
-	return fun.apply(null,arg)
+		arg.push(nxtfun())		// make a arg list
+	return fun.apply(null,arg)	// call and return
+	// TODO store the stack when EOF to let users call next with other code part
 }
+
+// interface
 function evil(pv,pa){
 	var rv
 	setv(pv)
@@ -47,13 +82,15 @@ function evil(pv,pa){
 		seta(pa)
 	try{
 		while(true)
-			rv=nxtfun()
+			rv=nxtfun()				// eval again and again
 	}catch(err){
-		if(!(err instanceof EOF))
+		if(!(err instanceof EOF))	// EOF considered not a error
 			throw err
 	}
-	return rv
+	return rv						// rv is the last called function's return val
 }
+
+// use get/set to control users not to break things
 module.exports=new class{
 	get EvilError(){
 		return EvilError
